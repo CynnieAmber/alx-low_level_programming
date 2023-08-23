568d670a7bcaabaa9b4b35136b7f48127b349786
CynnieHyamber <Hyamber053@gmail.com>
2023-08-22 22:12:31 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * print_dog - Prints a struct dog.
+ * @d: The struct dog to be printed.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+	if (d->name == NULL)
+		d->name =  "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+printf("Name: %s\n", (*d).name);
+printf("Age: %f\n", (*d).age);
+printf("Owner: %s\n", (*d).owner);
+	}
+	}
